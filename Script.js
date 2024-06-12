4d6c222a291a6d6dae20ff020a909f41b105baed
2SPy1 <151692033+2SPy1@users.noreply.github.com>
2024-06-12 16:56:08 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -41,3 +41,29 @@ let currentIndex = 0;
 
     setInterval(showNextSlide, 5000); // Intervalo de cambio 5s
 }
+
+// Obtener el modal
+var modal = document.getElementById("myModal");
+
+// Obtener el div que abre el modal
+var div = document.getElementById("modalBtn");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("closeModal")[0];
+
+// Cuando el usuario hace clic en el div, se abre el modal
+div.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Cuando el usuario hace clic en <span> (x), se cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic fuera del modal, se cierra el modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
